8169b49035778901a3abf5b899f1b90d3aa5ddb1
StevenRyan89757 <ruan_xuewu@163.com>
2017-04-15 18:15:35 +0800

    Add files via upload
    add huangcm test code

---
+++ haha.cpp
@@ -0,0 +1,15 @@
+// haha.cpp : 定义控制台应用程序的入口点。
+//
+
+
+#include <StdAfx.h>
+#include <iostream>
+
+using namespace std;
+int main(int argc, _TCHAR* argv[])
+{
+	
+	cout<<"hello world";
+	return 0;
+}
+
